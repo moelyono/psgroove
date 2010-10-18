65c53c83f8a4a501a21ad9b1f75081621af4d9a1
Tim Wu <tim.wu.0@gmail.com>
2010-10-18 02:07:38 -0700

    Fixed build_hex.sh to build for arduino boards

--- build_hex.sh
+++ build_hex.sh
@@ -6,18 +6,8 @@
 # License ("GPL") version 3, as published by the Free Software Foundation.
 #
 
-all_targets="teensy1 teensypp1 teensy2 teensypp2 \
-              at90usbkey minimus1 minimus32 maximus \
-              blackcat xplain olimex usbtinymkii \
-              bentio"
-
-avr_gcc_ver=`avr-gcc --version | head -n 1 | awk '{print $3}'`
-if [[ $avr_gcc_ver < "4.3.5" ]]; then
-  echo "OpenKubus compilation disabled. You need avr-gcc version 4.3.5 or later."
-  echo "(You currently have avr-gcc version ${avr_gcc_ver})"
-else
-  all_targets="${all_targets} openkubus"
-fi
+all_targets="mega duemilanove168 \
+             duemilanove168p duemilanove328p"
 
 i=1
 for target in ${all_targets}; do
@@ -25,76 +15,25 @@ for target in ${all_targets}; do
   let i++
 done
 
-mcu[$teensy1]=at90usb162
-board[$teensy1]=TEENSY
-mhz_clock[$teensy1]=16
-name[$teensy1]="Teensy 1.0"
+mcu[$mega]=atmega1280
+board[$mega]=ArduinoMega
+mhz_clock[$mega]=16
+name[$mega]="Arduino Mega"
 
-mcu[$teensypp1]=at90usb646
-board[$teensypp1]=TEENSY
-mhz_clock[$teensypp1]=16
-name[$teensypp1]="Teensy++ 1.0"
+mcu[$duemilanove168]=atmega168
+board[$duemilanove168]=ArduinoDuemilanove
+mhz_clock[$duemilanove168]=16
+name[$duemilanove168]="Arduino Duemilanove atmega168"
 
-mcu[$teensy2]=atmega32u4
-board[$teensy2]=TEENSY
-mhz_clock[$teensy2]=16
-name[$teensy2]="Teensy 2.0"
+mcu[$duemilanove168p]=atmega168p
+board[$duemilanove168p]=ArduinoDuemilanove
+mhz_clock[$duemilanove168p]=16
+name[$duemilanove168p]="Arduino Duemilanove atmega168p"
 
-mcu[$teensypp2]=at90usb1286
-board[$teensypp2]=TEENSY
-mhz_clock[$teensypp2]=16
-name[$teensypp2]="Teensy++ 2.0"
-
-mcu[$at90usbkey]=at90usb1287
-board[$at90usbkey]=USBKEY
-mhz_clock[$at90usbkey]=8
-name[$at90usbkey]="AT90USBKEY"
-
-mcu[$minimus1]=at90usb162
-board[$minimus1]=MINIMUS
-mhz_clock[$minimus1]=16
-name[$minimus1]="Minimus v1"
-
-mcu[$minimus32]=at90usb162
-board[$minimus32]=MINIMUS
-mhz_clock[$minimus32]=16
-name[$minimus32]="Minimus 32"
-
-mcu[$maximus]=at90usb162
-board[$maximus]=MAXIMUS
-mhz_clock[$maximus]=16
-name[$maximus]="Maximus"
-
-mcu[$blackcat]=at90usb162
-board[$blackcat]=BLACKCAT
-mhz_clock[$blackcat]=16
-name[$blackcat]="Blackcat"
-
-mcu[$xplain]=at90usb1287
-board[$xplain]=XPLAIN
-mhz_clock[$xplain]=8
-name[$xplain]="XPLAIN"
-
-mcu[$olimex]=at90usb162
-board[$olimex]=OLIMEX
-mhz_clock[$olimex]=8
-name[$olimex]="Olimex"
-
-mcu[$usbtinymkii]=at90usb162
-board[$usbtinymkii]=USBTINYMKII
-mhz_clock[$usbtinymkii]=16
-name[$usbtinymkii]="USBTINYMKII"
-
-mcu[$bentio]=at90usb162
-board[$bentio]=BENTIO
-mhz_clock[$bentio]=16
-name[$bentio]="Bentio"
-
-mcu[$openkubus]=atmega16u4
-board[$openkubus]=USBKEY
-mhz_clock[$openkubus]=8
-name[$openkubus]="OpenKubus"
-
+mcu[$duemilanove328p]=atmega328p
+board[$duemilanove328p]=ArduinoDuemilanove
+mhz_clock[$duemilanove328p]=16
+name[$duemilanove328p]="Arduino Duemilanove atmega328p"
 
 while [ "x$1" != "x" ]; do
   targets="$targets ${1}"
